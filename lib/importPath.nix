48dc430da07e67649e1740afd1a18d43bda5bdfa
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-30 22:33:38 +0100

    Fix: default.nix module now takes name of parent

--- lib/importPath.nix
+++ lib/importPath.nix
@@ -1,33 +1,45 @@
 { config, inputs, lib, overlays, pkgs, self, unstable, userInfo, util, ... }:
 let
-  mkModule = prefix: path: name: enabled:
-    let fname = lib.removeSuffix ".nix" (builtins.baseNameOf name);
-    in {
+  mkModule = { prefix, path, name, }: {
       options = {
-        "${prefix}${fname}Cfg".enable = lib.mkOption {
+        "${prefix}${name}Cfg".enable = lib.mkOption {
           type = lib.types.bool;
-          default = enabled;
-          description = "${fname} configuration";
+          default = true;
+          description = "${name} configuration";
         };
       };
-      config = lib.mkIf config."${prefix}${fname}Cfg".enable
-        (builtins.import "${path}/${name}" {
+      config = lib.mkIf config."${prefix}${name}Cfg".enable
+        (builtins.import path {
           inherit config inputs lib overlays pkgs self unstable userInfo util;
         });
     };
 
-  mkModuleWrapper = prefix: path: name:
+  mkModuleWrapper = uprefix: path: name:
     let
       match = builtins.toString path |> builtins.match ".*(all|darwin|linux).*";
       suffixMatch = suf: lib.hasSuffix suf userInfo.system;
       mkIfMatch = sysType: match == [sysType];
       mkIfAllMatch = sysType: mkIfMatch sysType && suffixMatch sysType;
+      isDefault = name == "default.nix";
+      combined = {
+        prefix = if isDefault
+          then lib.splitString "-" uprefix
+            |> lib.lists.reverseList
+            |> lib.lists.drop 1
+            |> lib.lists.reverseList
+            |> lib.strings.concatStringsSep "-"
+          else uprefix;
+        path = "${path}/${name}";
+        name = if isDefault
+          then ""
+          else builtins.baseNameOf name |> lib.removeSuffix ".nix";
+      };
     in match != null
         |> (isMatch:
           if !isMatch then null
-          else if mkIfMatch "all" then [ (mkModule prefix path name true) ]
-          else if mkIfAllMatch "darwin" then [ (mkModule prefix path name true) ]
-          else if mkIfAllMatch "linux" then [ (mkModule prefix path name true) ]
+          else if mkIfMatch "all" then [ (mkModule { inherit (combined) prefix path name; }) ]
+          else if mkIfAllMatch "darwin" then [ (mkModule { inherit (combined) prefix path name; }) ]
+          else if mkIfAllMatch "linux" then [ (mkModule  { inherit (combined) prefix path name; }) ]
           else null);
 
   checkDirRec = path: prefix:

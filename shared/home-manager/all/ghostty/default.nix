8f916fc60cd7c463c46bb09e122adc85d8882864
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-01-05 06:11:19 +0100

    undo auto link

--- shared/home-manager/all/ghostty/default.nix
+++ shared/home-manager/all/ghostty/default.nix
@@ -1,9 +1,1 @@
-{ lib, userInfo, ... }: {
-  xdg.configFile."ghostty/config" = { source = ./config; };
-
-  # Symlink to applications dir if darwin
-  home.file."Library/Application Support/com.mitchellh.ghostty/config".source =
-    lib.mkIf
-    (userInfo.system == "x86_64-darwin" || userInfo.system == "aarch64-darwin")
-    ./config;
-}
+{ ... }: { xdg.configFile."ghostty/config" = { source = ./config; }; }

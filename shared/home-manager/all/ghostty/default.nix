1877051c67f4dd6654c2829a2ef4ba3347cff469
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-01-05 06:05:25 +0100

    automatically symlink to mac config

--- shared/home-manager/all/ghostty/default.nix
+++ shared/home-manager/all/ghostty/default.nix
@@ -1,1 +1,9 @@
-{ ... }: { xdg.configFile."ghostty/config" = { source = ./config; }; }
+{ lib, userInfo, ... }: {
+  xdg.configFile."ghostty/config" = { source = ./config; };
+
+  # Symlink to applications dir if darwin
+  home.file."Library/Application Support/com.mitchellh.ghostty/config".source =
+    lib.mkIf
+    (userInfo.system == "x86_64-darwin" || userInfo.system == "aarch64-darwin")
+    ./config;
+}

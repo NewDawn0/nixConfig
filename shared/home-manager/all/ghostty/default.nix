33feba685202b5324944326b6e298ec5e84f6f7d
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-02 14:18:34 +0100

    fix ghostty config

--- shared/home-manager/all/ghostty/default.nix
+++ shared/home-manager/all/ghostty/default.nix
@@ -1,1 +1,6 @@
-{ ... }: { xdg.configFile."ghostty/config" = { source = ./config; }; }
+{ userInfo, lib, ... }: {
+  xdg.configFile."ghostty/config" = { source = ./config; };
+  home.file."Library/Application Support/com.mitchellh.ghostty/config" =
+    lib.mkIf (userInfo.system == "x86_64-darwin" || userInfo.system
+      == "aarch64-darwin") { source = ./config; };
+}
